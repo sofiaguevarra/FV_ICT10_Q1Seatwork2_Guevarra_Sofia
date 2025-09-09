4760bf52b7ecee5e51e069f46a4c87c399810f8e
sofiaguevarra <guevarra.sr@obmontessori.edu.ph>
2025-09-09 19:20:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyscript import display
+
+store_name = "Sofi's Books" #string
+owner_name = "Sofia Francesca Guevarra" #string
+year_founded = "2025" #string
+popular_item_price = "PHP 200" #string
+has_delivery = True #Boolean
+product_names = ['Animal Farm', 'Noli Me Tangere', 'Dekada 70'] #list
+book_prices = ['PHP 200', 'PHP 300', 'PHP 400'] #list
+
+display(store_name, target="store_name")
+display(owner_name, target="owner_name")
+display(year_founded, target="year_founded")
+display(business_hours, target="business_hours")
+display(popular_item_price, target="popular_item_price")
+display(has_delivery, target="has_delivery")
+display(product_names, target="product_names")
+display(book_prices, target="book_prices")
